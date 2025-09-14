fcbbbd4c25c84a058c762210951189b6426caf03
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-09-14 11:53:47 +0700

    fix: sync time with jwt

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
       const user = await this.authService.signIn(req.body);
 
       res.cookie("token", user.token, {
-        maxAge: 24 * 60 * 60 * 1000,
+        maxAge: 2 * 24 * 60 * 60 * 1000,
       });
       res.status(StatusCodes.OK).json(user);
     } catch (error: unknown) {
