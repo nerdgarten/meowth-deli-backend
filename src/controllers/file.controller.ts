3b5cccab50f4b3bff7a3b6e95bd37e19d8cdeca4
Deammy <deammykruthnim@gmail.com>
2025-09-15 16:13:26 +0700

    feat/upload_file

---
+++ src/controllers/file.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+import FileService from "@/services/file.service";
+import { AppError } from "@/types/error";
+
+export class FileController {
+    private fileService: FileService;
+    constructor() {
+        this.fileService = new FileService();
+    }
+
+    // Restaurant verification endpoints
+    async uploadFile(req: Request, res: Response) {
+        try {
+            const file = req.file;
+            res.status(StatusCodes.OK).json({ message: "File uploaded successfully", file });
+        } catch (error: unknown) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json({ message: error.message });
+                return;
+            }
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
+        }
+    }
+}
