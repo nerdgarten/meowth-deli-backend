96562d214b5cb2a7eb3282da4e9a7e39f13e7173
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2025-09-15 20:29:17 +0700

    fix conflict

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,8 @@ import { Router } from "express";
 
 import { AdminRouter } from "@/routes/admin.route";
 import { AuthRouter } from "@/routes/auth.route";
-<<<<<<< HEAD
-import { ProfileRouter } from "@/routes/profile.route";
 import { RestaurantRouter } from "@/routes/restaurant.route";
-=======
 import { CustomerRouter } from "@/routes/customer.route";
->>>>>>> 55f873d0883f18680e6c263a9a8a69db3fb20706
 import { TestRouter } from "@/routes/test.route";
 
 import { DishRouter } from "./dish.route";
@@ -30,12 +26,8 @@ export class RouterManager {
 
     this.router.use("/auth", authRouter.getRouter());
     this.router.use("/admin", adminRouter.getRouter());
-<<<<<<< HEAD
     this.router.use("/restaurant", restaurantRouter.getRouter());
-    this.router.use("/profile", profileRouter.getRouter());
-=======
     this.router.use("/customer", customerRouter.getRouter());
->>>>>>> 55f873d0883f18680e6c263a9a8a69db3fb20706
     this.router.use("/api-test", testRouter.getRouter());
     this.router.use("/dish", dishRouter.getRouter());
   }
