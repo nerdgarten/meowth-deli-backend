e0ffdc8953d5293aabc256eba0a3188d0f9d50b2
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-09-15 23:39:53 +0700

    fix: origin

--- src/app.ts
+++ src/app.ts
@@ -8,21 +8,7 @@ import { RouterManager } from "./routes";
 import type { Express } from "express";
 
 const corsOptions: CorsOptions = {
-  origin: (
-    origin: string | undefined,
-    callback: (err: Error | null, allow?: boolean | string) => void,
-  ) => {
-    const borworntatRegex = /^https:\/\/([a-zA-Z0-9-]+\.)*borworntat\.com$/;
-    if (
-      !origin ||
-      origin.startsWith("http://localhost") ||
-      borworntatRegex.test(origin)
-    ) {
-      callback(null, origin);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
   optionsSuccessStatus: 200,
@@ -36,10 +22,9 @@ app.use((req, res, next) => {
 });
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json());
-
-app.options("*", cors(corsOptions));
 
 const routerManager = new RouterManager();
 
