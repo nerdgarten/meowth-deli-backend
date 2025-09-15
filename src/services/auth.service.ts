730867069b955e589c936ddb5027898368d7db3b
Deammy <deammykruthnim@gmail.com>
2025-09-15 19:58:19 +0700

    fix:auth_service_bug

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -30,7 +30,7 @@ export default class AuthService {
 
     const userRoles = await this.authRepository.getUserRoles(user.id);
     
-    if (!(userRoles.some((userRole) => userRole.role == "customer"))) {
+    if (!(userRoles.some((userRole) => userRole.role == role))) {
       throw new AppError("Unauthorized role", StatusCodes.UNAUTHORIZED);
     }
 
